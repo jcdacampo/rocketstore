a95596c339ac5462fc9db0ff66c7adc4f9fa389d
jean <jcdacampo@unochapeco.edu.br>
2018-08-17 13:23:33 -0300

    Construindo o a seção: produtos por até

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -29,27 +29,61 @@
             <span>Descontos 30%</span>
           </div>
         </div>
-        <div class="column is-half-mobile is-paddingless desconto_terceiro">
+        <div class="column is-full-mobile is-paddingless desconto_terceiro">
           <img src="http://via.placeholder.com/200x140">
           <div class="desconto_desc is-vertical-center has-text-centered">
             <span>Descontos 20%</span>
           </div>
         </div>
-        <div class="column is-half-mobile is-paddingless desconto_quarto">
+        <div class="column is-full-mobile is-paddingless desconto_quarto">
           <img src="http://via.placeholder.com/200x140">
           <div class="desconto_desc is-vertical-center has-text-centered">
             <span>Descontos 50%</span>
           </div>
         </div>
       </section>
-      <section class="columns is-multiline promocoes">
-        <div class="column promocoes_desc is-3 has-text-centered is-vertical-center">
+      <section class="columns is-multiline promocoes is-vertical-center">
+        <div class="column promocoes_desc is-2-desktop is-12-mobile has-text-centered is-vertical-center">
           <span><b>Promoções</b> do dia </span>
         </div>
-        <div class="column promocoes_imagens is-flex is-9">
+        <div class="column promocoes_img is-3-desktop is-3-mobile is-vertical-center">
           <img src="http://via.placeholder.com/180x140">
+        </div>
+        <div class="column promocoes_img is-3-desktop is-3-mobile is-vertical-center">
           <img src="http://via.placeholder.com/180x140">
+        </div>
+        <div class="column promocoes_img is-3-desktop is-3-mobile is-vertical-center">
           <img src="http://via.placeholder.com/180x140">
+        </div>
+      </section>
+      <section class="columns is-multiline is-mobile produtos_ate">
+        <div class="column has-text-centered">
+          <div class="produtos_ate_box">
+            <span class="produtos_ate_box_desc">Produtos</span>
+            <p>por até</p>
+            <b>R$ 59,90</b>
+          </div>
+        </div>
+        <div class="column has-text-centered">
+          <div class="produtos_ate_box">
+            <span class="produtos_ate_box_desc">Produtos</span>
+            <p>por até</p>
+            <b>R$ 99,90</b>
+          </div>
+        </div>
+        <div class="column has-text-centered">
+          <div class="produtos_ate_box">
+            <span class="produtos_ate_box_desc">Produtos</span>
+            <p>por até</p>
+            <b>R$ 159,90</b>
+          </div>
+        </div>
+        <div class="column has-text-centered">
+          <div class="produtos_ate_box">
+            <span class="produtos_ate_box_desc">Produtos</span>
+            <p>por até</p>
+            <b>R$ 259,90</b>
+          </div>
         </div>
       </section>
     </div>
