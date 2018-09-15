43fe0e46f37230fd2278078dc75b1553cb1fe4fd
jean <jcdacampo@unochapeco.edu.br>
2018-09-15 19:16:14 -0300

    Página de produto, seção de ações da compra.

--- src/components/Produto.vue
+++ src/components/Produto.vue
@@ -9,20 +9,90 @@
       </ul>
     </nav>
     <div class="produto columns">
-      <div class="column is-5 produto_imagem">
+      <div class="column is-6 produto_imagem">
         <div class="columns">
           <div class="column is-3 produto_imagem_miniaturas">
             <img class="imagem miniatura active" src="/static/images/produto01.jpg">
             <img class="imagem miniatura" src="/static/images/produto02.jpg">
             <img class="imagem miniatura" src="/static/images/produto03.png">
           </div>
-          <div class="column produto_imagem_principal">
+          <div class="column produto_imagem_principal has-text-centered">
             <img class="imagem principal" id="zoom_image" src="/static/images/produto01.jpg" data-zoom-image="/static/images/produto01.jpg">
+            <button class="button"><i class="fas fa-heart"></i>Adicionar aos favoritos</button>
+            <button class="button"><i class="fas fa-bell"></i> Alerta de preço</button>
+            <button class="button"><i class="fas fa-exchange-alt"></i>Comparar</button>
           </div>
         </div>
       </div>
-      <div class="column is-7 produto_desc">
-        desc
+      <div class="column is-6 produto_desc">
+        <div class="produto_desc_titulo is-flex">
+          <h1>iPhone X 256gb Silver</h1>
+          <div class="produto_desc_titulo_avaliacao">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+          </div>
+        </div>
+        <hr>
+        <div class="produto_desc_preco">
+          <div class="linha_um is-flex is-space-between">
+            <div class="produto_desc_preco_antigo">
+              <strike>R$ 5999,00</strike>
+            </div>
+            <div class="produto_desc_preco_porcentagem">
+              <span>-17%</span>
+            </div>
+          </div>
+          <div class="linha_dois">
+            <div class="produto_desc_preco_atual_boleto">
+              <i class="fas fa-barcode"></i>
+              <span>R$ 4999,00</span> á vista no boleto
+            </div>
+          </div>
+          <div class="linha_tres">
+            <div class="produto_desc_preco_atual_cartao_prazo">
+              <i class="fas fa-credit-card"></i>
+              <span>R$ 5290,00</span> em até 5x de <b>R$ 1058,00</b> sem juros
+            </div>
+            ou
+            <div class="produto_desc_preco_atual_cartao_prazo_uma is-flex is-space-between">
+              <div>
+                <i class="fas fa-credit-card"></i>
+                <span>R$ 5050,00</span> em <b>1x</b> no cartão
+              </div>
+              <a>Ver parcelas</a>
+            </div>
+          </div>
+        </div>
+        <hr>
+        <div class="produto_desc_acoes is-flex">
+          <div class="produto_desc_acoes_cor">
+            Cor
+            <div></div>
+          </div>
+          <div class="produto_desc_acoes_qtd">
+            Quantidade
+            <div class="seletor_quantidade">
+              <div class="field">
+                <div class="control">
+                  <div class="select">
+                    <select>
+                      <option>1</option>
+                      <option>2</option>
+                      <option>3</option>
+                      <option>4</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="produto_desc_acoes_comprar">
+
+          </div>
+        </div>
       </div>
     </div>
   </div>
