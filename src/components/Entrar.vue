4e16d215af8b623830a84a171e6b37e6ab94730b
jean <jcdacampo@unochapeco.edu.br>
2018-09-29 17:10:17 -0300

    Pagina de cadastro

--- src/components/Entrar.vue
+++ src/components/Entrar.vue
@@ -1,5 +1,60 @@
 <template>
-  <h1>Entrar</h1>
+  <div class="entrar columns is-multiline">
+    <nav class="breadcrumb" aria-label="breadcrumbs">
+      <ul>
+        <li><a href="#">Início</a></li>
+        <li class="is-active"><a href="#" aria-current="page">Entrar</a></li>
+      </ul>
+    </nav>
+    <span class="column is-12 has-text-centered aviso"><i class="fa fa-lock"></i>A rocketstore <b>nunca</b> irá pedir seus dados de acesso.</span>
+    <div class="column cliente is-vertical-center">
+      <h1 class="has-text-centered">Já sou cliente cliente Rocketstore</h1>
+      <div class="field">
+        <p class="control has-icons-left">
+          <input class="input" type="email" placeholder="Digite seu email ou CPF">
+          <span class="icon is-small is-left">
+            <i class="fas fa-envelope"></i>
+          </span>
+        </p>
+      </div>
+      <div class="field">
+        <p class="control has-icons-left">
+          <input class="input" type="password" placeholder="Digite sua senha">
+          <span class="icon is-small is-left">
+            <i class="fas fa-lock"></i>
+          </span>
+        </p>
+      </div>
+      <label class="checkbox">
+        <input type="checkbox">
+        Entrar automaticamente
+      </label>
+      <button class="button">
+        Entrar
+      </button>
+      <a>Esqueci minha senha</a>
+      <span class="entrarcomspan">Entrar com</span>
+      <div class="entrarcom">
+        <i class="fab fa-google"></i>
+        <i class="fab fa-facebook-f"></i>
+      </div>
+    </div>
+    <div class="is-divider-vertical" data-content="OU"></div>
+    <div class="column naocliente is-vertical-center">
+      <h1 class="has-text-centered">Quero ser cliente Rocketstore</h1>
+      <div class="field">
+        <p class="control has-icons-left">
+          <input class="input" type="email" placeholder="Digite seu email">
+          <span class="icon is-small is-left">
+            <i class="fas fa-envelope"></i>
+          </span>
+        </p>
+      </div>
+      <router-link class="button" to="/registrar">
+          Continuar cadastro
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script>
--- src/components/Registrar.vue
+++ src/components/Registrar.vue
@@ -1,5 +1,111 @@
 <template>
-  <h1>Registrar</h1>
+  <div class="registrar is-multiline">
+    <nav class="breadcrumb" aria-label="breadcrumbs">
+      <ul>
+        <li><a href="#">Início</a></li>
+        <li class="is-active"><a href="#" aria-current="page">Registrar</a></li>
+      </ul>
+    </nav>
+    <div class="registrar_dados is-vertical-center">
+      <h1>Nos informe alguns dados</h1>
+
+      <div class="registrar_dados_campos">
+        <div class="field">
+          <label>Seu email:</label>
+          <p class="control has-icons-left">
+            <input class="input" type="email" placeholder="Email">
+            <span class="icon is-small is-left">
+              <i class="fas fa-envelope"></i>
+            </span>
+          </p>
+        </div>
+        <div class="field">
+          <label>Defina uma senha:</label>
+          <p class="control has-icons-left">
+            <input class="input" type="email" placeholder="Senha">
+            <span class="icon is-small is-left">
+              <i class="fas fa-lock"></i>
+            </span>
+          </p>
+        </div>
+        <div class="field">
+          <label>Repita a senha:</label>
+          <p class="control has-icons-left">
+            <input class="input" type="email" placeholder="Confirme a senha">
+            <span class="icon is-small is-left">
+              <i class="fas fa-lock"></i>
+            </span>
+          </p>
+        </div>
+      </div>
+
+      <div class="registrar_dados_campos">
+        <div class="field">
+          <label>Informe seu nome:</label>
+          <p class="control has-icons-left">
+            <input class="input" type="email" placeholder="Nome">
+            <span class="icon is-small is-left">
+              <i class="fas fa-user"></i>
+            </span>
+          </p>
+        </div>
+        <div class="field">
+          <label>Informe seu CPF:</label>
+          <p class="control has-icons-left">
+            <input class="input" type="email" placeholder="CPF">
+            <span class="icon is-small is-left">
+              <i class="fas fa-user"></i>
+            </span>
+          </p>
+        </div>
+        <div class="field metade">
+            <label>Data de nascimento:</label>
+            <p class="control has-icons-left">
+              <input class="input" type="email" placeholder="Data de nascimento">
+              <span class="icon is-small is-left">
+              <i class="fas fa-user"></i>
+            </span>
+            </p>
+          </div>
+        <div class="field metade">
+            <label>Sexo:</label>
+            <label class="radio">
+              <input type="radio" name="answer">
+              Masculino
+            </label>
+            <label class="radio">
+              <input type="radio" name="answer">
+              Feminino
+            </label>
+
+          </div>
+
+
+      </div>
+
+      <div class="registrar_dados_campos">
+        <div class="field">
+          <label>Telefone:</label>
+          <p class="control has-icons-left">
+            <input class="input" type="email" placeholder="Telefone">
+            <span class="icon is-small is-left">
+              <i class="fas fa-phone"></i>
+            </span>
+          </p>
+        </div>
+        <div class="field">
+          <label>Informe seu CEP:</label>
+          <p class="control has-icons-left">
+            <input class="input" type="email" placeholder="CEP">
+            <span class="icon is-small is-left">
+              <i class="fas fa-home"></i>
+            </span>
+          </p>
+        </div>
+      </div>
+
+    </div>
+  </div>
 </template>
 
 <script>
